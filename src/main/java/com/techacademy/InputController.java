42b65cacabe64848053763f20bd9eceaa5bb55b6
Remy999 <vermilionseraph@gmail.com>
2024-01-21 16:46:42 +0900

    コミット

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -2,14 +2,15 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
 
     @GetMapping("/input")
-    public String getInput(Model model) {
-        // 入力画面の初期表示のための処理
-        // ここで必要なデータをModelに追加してください
+    public String getInput(@RequestParam(name = "previous", required = false) String previous, Model model) {
+        // previous パラメータをモデルに追加して入力画面を表示
+        model.addAttribute("previous", previous);
         return "input";
     }
 }
--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -2,6 +2,7 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -9,9 +10,15 @@ public class OutputController {
 
     @PostMapping("/output")
     public String postOutput(@RequestParam("inputValue") String inputValue, Model model) {
-        // 入力値を受け取って出力画面を表示するための処理
-        // ここで必要な処理やデータを行い、結果をModelに追加してください
-        model.addAttribute("outputValue", "入力された値は「 " + inputValue +"」です");
+        // 入力値をモデルに追加して出力画面を表示
+        model.addAttribute("inputValue", inputValue);
         return "output";
+    }
+
+    @RequestMapping("/input")
+    public String redirectToInput(@RequestParam("previous") String previous, Model model) {
+        // 入力画面にリダイレクトする際に previous パラメータを渡す
+        model.addAttribute("previous", previous);
+        return "redirect:/input";
     }
 }
